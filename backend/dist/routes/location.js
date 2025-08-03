d797800a4600cea32c2e6a7da77f12047b803460
Lisa Nguyen <ngocanhthu.work@gmail.com>
2025-08-03 01:13:04 -0400

    fix auth check and add location route

---
+++ backend/dist/routes/location.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const locationController_1 = require("../controllers/locationController");
+const router = (0, express_1.Router)();
+router.get('/reverse-geocode', locationController_1.reverseGeocode);
+router.get('/emergency', locationController_1.getEmergencyFacilities);
+exports.default = router;
+//# sourceMappingURL=location.js.map
